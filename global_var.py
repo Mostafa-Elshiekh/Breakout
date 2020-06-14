6f531c8635c81dbe54908f85f6d60d8cff6b5205
Mostafa Ebrahim <mostafa.ebrahim.011@hotmail.com>
2020-06-14 06:31:37 +0200

    Add files via upload

--- global_var.py
+++ global_var.py
@@ -3,24 +3,14 @@ FROM_LEFT = 2
 FROM_TOP = 3
 FROM_BOTTOM = 4
 
+mouse_x = 0
+
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 600
 
-#array=[0]
-# numbers list
-numbers =0
-
-# use for loop to fill numbers list with elements
-'''for i in range(0,40):
-    numbers.append(0)'''
-
-
-playerResult=0
-
+playerResult = 0
 
 deltaX = 1
 deltaY = 1
 
-
-
-time_interval =5 # try  500 msec
+time_interval = 2
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from functions import*
+from functions import *
 from OpenGL.GLUT import *
-from global_var import*
+from global_var import *
+
 
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-    # mouse coordinates inbetween [WINDOW_WIDTH=800,WINDOW_HEIGHT=500]
-    # glutInitWindowSize (1100, 600) # try and notice the bat; mouse coordinates inbetween [1100,600] (where 1100 pixels = 800 in openGL)
+    glutInitWindowSize(500, 600)
     glutInitWindowPosition(0, 0)
-    glutCreateWindow(b"Super Breakout OpenGL game")
+    glutCreateWindow(b"Super Breakout")
     glutDisplayFunc(Display)
     glutTimerFunc(time_interval, Timer, 1)
     glutPassiveMotionFunc(MouseMotion)
