b21b5f54320e0d5b5a73bedf3c9de458c83d4a6a
Mostafa Ebrahim <mostafa.ebrahim.011@hotmail.com>
2020-06-14 06:30:39 +0200

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,4 +1,3 @@
-from global_var import *
 class RectLevel:
     def __init__(self, left, bottom, right, top):
         self.left = left
@@ -6,26 +5,22 @@ class RectLevel:
         self.right = right
         self.top = top
 
-    # creating list
-me=[]
-#me=RectLevel(435,444,475,460)
-#me1=RectLevel(395,444,420,460)
 
-#new.append(RectLevel(435,444,475,460))
-count=0
-count_y = 30   # counter to shift the rect to the higher level in y axis
-count_x=0      #counter to shift the rect to the second rect in x axis(At the same level(
+# creating list
+me = []
+count = 0
+count_y = 30  # counter to shift the rect to the higher level in y axis
+count_x = 0  # counter to shift the rect to the second rect in x axis(At the same level(
 
-for k in range(0,4):
- for j in range(0,10):
-    me.append(RectLevel(435-count_x, 444+count_y, 475-count_x, 460+count_y))
+for k in range(0, 10):
+    for j in range(0, 10):
+        me.append(RectLevel(435 - count_x, 440 + count_y, 475 - count_x, 460 + count_y))
+        count_x += 45.5
+    count_y += 30  # increment y to go higher level
+    count_x = 0  # make it 0 to return to the start of level
 
-    count_x+=45
 
- count_y += 30 #increment y to go higher level
- count_x=0 #make it 0 to return to the start of level
-
-#class to define attributes for Bat and boll
+# class to define attributes for Bat and boll
 class RECTA:
     def __init__(self, left, bottom, right, top):
         self.left = left
